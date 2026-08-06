b0dc78870b307d1e5a54754b22b1612580c177ac
snehaparvatham <84832901+lucky8181681@users.noreply.github.com>
2026-08-06 18:08:38 +0530

    Create Student.cs

---
+++ 02-Classes-and-Objects/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp_Csharp.Day2
+{
+    internal class Student
+    {
+        public string Name;
+        public int Id;
+
+        public Student(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Name: {Name}, Id: {Id}");
+
+        }
+        public void UpdateDetails(string newName,int newId)
+        {
+            Name = newName;
+            Id = newId;
+        }
+    }
+}
