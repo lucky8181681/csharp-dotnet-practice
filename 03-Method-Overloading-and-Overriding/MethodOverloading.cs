8537130c374b68fff210cf95e0db2eb611d74386
snehaparvatham <84832901+lucky8181681@users.noreply.github.com>
2026-08-06 18:19:29 +0530

    Create MethodOverloading.cs

---
+++ 03-Method-Overloading-and-Overriding/MethodOverloading.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day3
+{
+    class MethodOverloading
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+}
