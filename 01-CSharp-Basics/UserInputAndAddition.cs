c20601888a94a4dba20d0efa56e0f2d3ed5d87b8
snehaparvatham <84832901+lucky8181681@users.noreply.github.com>
2026-08-06 17:48:37 +0530

    Create UserInputAndAddition.cs

---
+++ 01-CSharp-Basics/UserInputAndAddition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        Console.WriteLine("Sum is: " + sum);
+
+        Console.Write("Enter something: ");
+        string input = Console.ReadLine();
+
+        Console.WriteLine("You entered: " + input);
+
+        Console.ReadLine();
+    }
+}
