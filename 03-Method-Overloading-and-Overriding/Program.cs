a2cdccf8fefe61b05e877dd4c78a3b0c92f1e27c
snehaparvatham <84832901+lucky8181681@users.noreply.github.com>
2026-08-06 18:21:01 +0530

    Create Program.cs

---
+++ 03-Method-Overloading-and-Overriding/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Method Overloading
+            Console.WriteLine(MethodOverloading.Add(10, 20));
+            Console.WriteLine(MethodOverloading.Add(10, 20, 30));
+
+            // Method Overriding
+            Animal obj = new Dog();
+            obj.Sound();
+        }
+    }
+}
